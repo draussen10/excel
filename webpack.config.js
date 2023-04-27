aaf2eeb2d3d5440d037a965d481eacdbabe201f5
draussen10 <a.a.boev@bk.ru>
2023-04-27 16:34:13 +0300

    Added modes, clean's function and sourceMap

--- webpack.config.js
+++ webpack.config.js
@@ -1,57 +1,71 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const FaviconsWebpackPlugin = require('favicons-webpack-plugin')
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-module.exports = {
-	context: path.resolve(__dirname, 'src'), //Абсолютный путь до папки src
-	entry: {
-		main: ['@babel/polyfill', './index.js'] //Относительно context - папки src
-	},
-	output: {
-		path: path.resolve(__dirname, 'dist'),
-		filename: '[name].bundle.js'
-	},
-	resolve: {
-		extensions: ['.js'], //Чтобы не писать на конце .js
-		alias: {
-			'@': path.resolve(__dirname, 'src'), //Чтобы не писать './././'
-			'@core': path.resolve(__dirname, 'core')
-		}
-	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			template: "./index.html" //Относительно context - папки src
-		}),
-		new FaviconsWebpackPlugin({
-			logo: './favicon.ico',
-		}),
-		new MiniCssExtractPlugin({  //Преобразует и минифицирукт SCSS
-			filename: '[name].bundle.css'
-		})
-	],
-	module: {//Чтобы работать с разными файлами, не только с js - loader
-		rules: [
-			{
-				test: /\.s[ac]ss$/i,
-				use: [
-					MiniCssExtractPlugin.loader, //Преобразует и минифицирукт SCSS
-					// Translates CSS into CommonJS
-					"css-loader",
-					// Compiles Sass to CSS
-					"sass-loader",
+
+module.exports = (env, argv) => {
+
+	const isProd = argv.mode === 'production'
+	const isDev = !isProd
+
+	const filename = ext => `[name].${isProd ? '[contenthash]' : ''}.bundle.${ext}`
+
+	return {
+		context: path.resolve(__dirname, 'src'), //Абсолютный путь до папки src
+		entry: {
+			main: ['@babel/polyfill', './index.js'] //Относительно context - папки src
+		},
+		output: {
+			path: path.resolve(__dirname, 'dist'),
+			filename: filename('js')
+		},
+		resolve: {
+			extensions: ['.js'], //Чтобы не писать на конце .js
+			alias: {
+				'@': path.resolve(__dirname, 'src'), //Чтобы не писать './././'
+				'@core': path.resolve(__dirname, 'core')
+			}
+		},
+		plugins: [
+			new HtmlWebpackPlugin({
+				template: "./index.html" //Относительно context - папки src
+			}),
+			new CopyPlugin({ //Перенести файлы с src в dist
+				patterns: [
+					{ from: path.resolve(__dirname, 'src', 'favicon.ico'), to: path.resolve(__dirname, 'dist') },
 				],
-			},
-			{ // Babel
-				test: /\.m?js$/,
-				exclude: /node_modules/,
-				use: {
-					loader: "babel-loader",
-					options: {
-						presets: ['@babel/preset-env']
+			}),
+			new MiniCssExtractPlugin({  //Преобразует и минифицирует SCSS
+				filename: filename('css')
+			}),
+			new CleanWebpackPlugin(), //Чистка папки dist
+		],
+		devtool: isDev ? 'source-map' : false, //Для удобного отслеживания -  в DevTools будет отображать расположение нужного файла в папке src, а не в папке сборке dist
+		module: {//Чтобы работать с разными файлами, не только с js - loader
+			rules: [
+				{
+					test: /\.s[ac]ss$/i,
+					use: [
+						MiniCssExtractPlugin.loader, //Преобразует и минифицирукт SCSS
+						// Translates CSS into CommonJS
+						"css-loader",
+						// Compiles Sass to CSS
+						"sass-loader",
+					],
+				},
+				{ // Babel
+					test: /\.m?js$/,
+					exclude: /node_modules/,
+					use: {
+						loader: "babel-loader",
+						options: {
+							presets: ['@babel/preset-env']
+						}
 					}
 				}
-			}
-		]
+			]
+		}
 	}
 }
